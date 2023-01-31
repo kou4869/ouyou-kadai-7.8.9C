4e5a40d290affbfa157c977255ad9e5f2d5748c4
EC2 Default User <ec2-user@ip-172-31-6-169.ap-northeast-1.compute.internal>
2023-01-31 09:28:11 +0000

    2回目のコミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  has_one_attached :profile_image
+  has_many :book_comment, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_one_attached :profile_image
+  
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
